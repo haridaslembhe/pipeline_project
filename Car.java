3390c1f98db5bacb4013ccd1f1120554523a36db
haridaslembhe <125847670+haridaslembhe@users.noreply.github.com>
2025-09-04 14:12:49 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,24 @@
+class Car {
+    String brand;
+    int year;
+
+    // Constructor
+    Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    void displayInfo() {
+        System.out.println("Car: " + brand + " (" + year + ")");
+    }
+}
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", 2020);
+        Car car2 = new Car("Honda", 2022);
+
+        car1.displayInfo();
+        car2.displayInfo();
+    }
+}
