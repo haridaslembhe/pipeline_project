1266edf4f4eabc663dda149bcb73fda8558d23f9
haridaslembhe <125847670+haridaslembhe@users.noreply.github.com>
2025-09-04 23:04:44 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -16,7 +16,7 @@ class Car {
 public class demo {
     public static void main(String[] args) {
         Car car1 = new Car("Toyota", 2020);
-        Car car2 = new Car("Honda", 2022);
+        Car car2 = new Car("Honda", 2025);
 
         car1.displayInfo();
         car2.displayInfo();
